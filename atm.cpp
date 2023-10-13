7c05cd78b74f640e6b803b66cdbaf0b41c9ba837
Guillermo Lopez <guillermo-lopez@guille.tech>
2023-10-13 18:20:34 +0000

    added rand

--- atm.cpp
+++ atm.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <cstdlib>
 
+using namespace std;
+
+int main()
+{
+    int initialBalance = rand() % 100 + 1;
+    cout << initialBalance << endl;
+    cout << "Hello World!";
+    return 0;
+}
